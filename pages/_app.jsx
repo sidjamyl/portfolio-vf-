e8fbe906e907d1b4eacbc78aeb5f990ac20a6285
jamyl <sidjamyl2006@gmail.com>
2025-08-31 14:12:21 +0100

    etahn

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,23 +1,33 @@
-import { AnimatePresence, motion } from "framer-motion";
-import { useRouter } from "next/router";
+import '../styles/globals.css';
+import Head from 'next/head';
 
-import Layout from "../components/Layout";
-import Transition from "../components/Transition";
+import Layout from '../components/Layout';
+import Transition from '../components/Transition';
 
-import "../styles/globals.css";
+import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <Layout>
-      <AnimatePresence mode="wait">
-        <motion.div key={router.route} className="h-full">
-          <Transition />
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-    </Layout>
+    <>
+      <Head>
+        <title>Portfolio</title>
+        <meta name="description" content="Portfolio de Sid Jamyl et Medjani Mehdi - Création de contenu digital" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <Layout>
+        <AnimatePresence mode="wait">
+          <motion.div key={router.route} className="h-full">
+            <Transition />
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </>
   );
 }
 
